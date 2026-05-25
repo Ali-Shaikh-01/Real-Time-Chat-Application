f097266c0658132b39320e3aeb34cd3b8b2f456a
Ali-Shaikh-01 <alimuzammil2008@gmail.com>
2026-05-25 23:08:34 +0530

    Test

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,10 +1,10 @@
 import { io } from "socket.io-client";
 
 const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3001";
-console.log("SERVER_URL:", SERVER_URL);
 // Factory — always creates a fresh socket (no singleton!)
 export function createSocket() {
   return io(SERVER_URL, {
     autoConnect: false,
+    transports: ["websocket"],
   });
 }
